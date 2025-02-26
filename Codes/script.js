175fab513322606774882fd564a920b22118757e
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-02-26 11:14:57 +0800

    d

---
+++ Codes/script.js
@@ -0,0 +1,12 @@
+function showPopup() {
+    document.getElementById('popup').style.display = 'flex';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+function goBack() {
+    // Implement back navigation logic here if needed
+    alert("Going back to the website layout...");
+}
